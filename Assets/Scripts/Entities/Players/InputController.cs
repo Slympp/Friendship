4c24bb02a10cf15e1f5981cfd6c0f91b15d30bea
Slympp <leo.slymp@gmail.com>
2019-06-16 22:33:36 +0200

    :tada: Init

---
+++ Assets/Scripts/Entities/Players/InputController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Entities.Players {
+    public static class InputController {
+        
+        public static Vector2 Movement => new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        public static Vector2 Shoot    => new Vector2(Input.GetAxisRaw("HorizontalShoot"), Input.GetAxisRaw("VerticalShoot"));
+    }
+}
---
+++ Assets/Scripts/Entities/Players/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Entities.Players {
+    
+    public class PlayerController : MonoBehaviour {
+        
+        private CharacterController2D _characterController;
+    
+        void Awake() {
+            _characterController = GetComponent<CharacterController2D>();
+        }
+
+        void FixedUpdate() {
+
+            Vector2 movement = InputController.Movement;
+            _characterController.Move(movement.x, movement.y < 0, movement.y > 0);
+        }
+    }
+}
